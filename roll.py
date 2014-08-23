06cd538320d59e3c163b61935392eaccacd63794
PanosFirmpas <panosfirbas@gmail.com>
2014-08-23 23:39:40 +0200

    ..
    Changed usage

--- roll.py
+++ roll.py
@@ -1,9 +1,10 @@
 import random
 import datetime
 import time
+import sys
 
 
-def get_roll(die,how_many):
+def get_roll(how_many,die):
 	die,how_many = int(die),int(how_many)
 	results = []
 	for d in range(how_many):
@@ -11,32 +12,55 @@ def get_roll(die,how_many):
 		results.append(result)
 	return results
 
+def update(operator,num1,num2):
+	if operator == '+':
+		return num1 + num2
+	elif operator == '-':
+		return num1 - num2
+	else:
+		sys.exit('Unrecognized operator')
+
+
 def roll():
-	message = '[{3}] {2}>> Rolled {1} d{0} dice and got: '
+	message = '[{2}] {1}>> Rolled {0} and got: '
 	r_number = 0
 	while 1:
-		arguments = raw_input('roll(die,how_many): ')
-		if arguments.endswith(' '):
-			arguments = arguments[:-1]
+		o_arguments = raw_input('roll(2d20+5): ')
+		arguments = o_arguments
 
-		arguments = arguments.split(' ')
+		for c in ['+','-',None]:
+			l = arguments.split(c)
+			if len(l) > 1:
+				arguments,modifier = l
+				break
+
+
+		# if arguments.endswith(' '):
+		# 	arguments = arguments[:-1]
+
+		arguments = arguments.split('d')
+		if len(arguments) != 2:
+			print "Looks like you made a typo. Usage: '2d20', or '2d20+5', or '2d20-3', or 'd20+6', or 'd20'"
+			continue
+		if arguments[0] == '':
+			arguments[0] = '1'
 		arguments = [int(x) for x in arguments]
-		
-		
-		
-		if len(arguments) == 1:
-			arguments.append(1)
-
 		
 
 		results = get_roll(*arguments)
+		
+		u_results = ''
+		if c:
+			u_results = []
+			for x in results:
+				u_results.append(update(c,x,int(modifier)))
 
 		ts = time.time()
 		dt = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
 		
 		
-		al = arguments+[dt,r_number]
+		al = [o_arguments, dt,r_number]
 
 
-		print message.format(*al), results
+		print message.format(*al), results, u_results
 		r_number += 1
